Fixed normalisation

rob/DataSet.py
@@ -1,6 +1,7 @@
 import numpy as np
 from six.moves import cPickle as pickle
-
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import normalize
 
 class DataSet():
 
@@ -10,9 +11,14 @@ class DataSet():
         self.labels = None
         self.image = None
 
-        self.valid_dataset
-        self.train_dataset
+        self.test_dataset =None
+        self.train_dataset = None
 
+        self.test_labels = None
+        self.train_labels = None
+
+        self._image_size = 32
+        self._num_channels = 3
 
     def unpickle(self, file):
         fo = open(file, 'rb')
@@ -24,22 +30,44 @@ class DataSet():
         return X.reshape((X.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
 
     def load(self):
+        # Load data files
         b1 = self.unpickle('cifar-10-batches-py/data_batch_1')
         b2 = self.unpickle('cifar-10-batches-py/data_batch_1')
         b3 = self.unpickle('cifar-10-batches-py/data_batch_1')
         b4 = self.unpickle('cifar-10-batches-py/data_batch_1')
         b5 = self.unpickle('cifar-10-batches-py/data_batch_1')
 
-        self.data = np.vstack([b1[b'data'], b2[b'data'], b3[b'data'], b4[b'data'], b5[b'data']])
+        print(b1[b'data'])
+
+        # Concatenate data
+        self.data = np.vstack([b1[b'data'], b2[b'data'], b3[b'data'], b4[b'data'], b5[b'data']]).astype(np.float32)
+
+        # Convert to image (for visualisation purposes)
         self.image = self.to_image(self.data)
-        print(self.data.shape)
 
-        self.labels = np.hstack([b1[b'labels'], b2[b'labels'], b3[b'labels'], b4[b'labels'], b5[b'labels']])
+        # Reformat
+        self.data = self.data.reshape((-1, self._image_size * self._image_size * self._num_channels)).astype(np.float32)
 
+        # Concatenate labels
+        labels = np.hstack([b1[b'labels'], b2[b'labels'], b3[b'labels'], b4[b'labels'], b5[b'labels']])
+        print('Labels: ', labels.shape)
+
+        # Reformat labels
+        num_labels = 10
+        self.labels = (np.arange(num_labels) == labels[:, None]).astype(np.float32)
+        print('Labels (2): ', self.labels.shape)
+
+        # Pull out the names of the labels
         meta = self.unpickle('cifar-10-batches-py/batches.meta')
-
         self.label_name = meta[b'label_names']
-        print(self.labels.shape)
+
+
+
+
+        self.data = normalize(self.data, axis=0)
+
+        self.train_dataset, self.test_dataset, self.train_labels, self.test_labels = train_test_split(self.data, self.labels, test_size=.33, random_state=10)
+
         #print(test[b'label_names'])
 
 

rob/baseline_nn.py
@@ -7,15 +7,23 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import roc_curve, auc
 from six.moves import cPickle as pickle
 from six.moves import range
+import time
+
 
 import tensorflow as tf
 
-import DataSet
+import DataSet as ds
+
 
 class Baseline:
 
     def __init__(self, ds):
         self._ds = ds
+
+        self.train_prediction = None
+        self.test_prediction = None
+
+        #self.test_preds = None
 
     def weight_variable(self, shape):
         # initial = tf.truncated_normal(shape, stddev=0.01)
@@ -27,35 +35,36 @@ class Baseline:
         initial = tf.zeros(shape)
         return tf.Variable(initial)
 
-    def run(self):
-        
-        batch_size = 256
-        hidden_nodes = 1024
+    def create(self, batch_size = 256, hidden_nodes = 1024, lamb_reg=0.01, start_learning_rate = 0.5):
+
+        self._batch_size = batch_size
 
         split_by_half = lambda x, k: int(x / 2 ** k)
 
-        graph = tf.Graph()
-        with graph.as_default():
+        input_layer_size = 32 * 32 * 3
+        output_layer_size = 10
+
+        self.graph = tf.Graph()
+        with self.graph.as_default():
             # Input data.
-            tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, 784))
-            tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, 10))
-            tf_valid_dataset = tf.constant(self._ds.valid_dataset)
-            tf_test_dataset = tf.constant(self._ds.test_dataset)
+            self.tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, input_layer_size))
+            self.tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, output_layer_size))
+            self.tf_test_dataset = tf.constant(self._ds.test_dataset)
 
             # Variables.
-            layer1_weights = self.weight_variable([784, hidden_nodes])
+            layer1_weights = self.weight_variable([input_layer_size, hidden_nodes])
             layer1_biases = self.bias_variable([hidden_nodes])
             layer2_weights = self.weight_variable([hidden_nodes, split_by_half(hidden_nodes, 1)])
             layer2_biases = self.bias_variable([split_by_half(hidden_nodes, 1)])
             layer3_weights = self.weight_variable([split_by_half(hidden_nodes, 1), split_by_half(hidden_nodes, 2)])
             layer3_biases = self.bias_variable([split_by_half(hidden_nodes, 2)])
-            layer4_weights = self.weight_variable([split_by_half(hidden_nodes, 2), 10])
-            layer4_biases = self.bias_variable([10])
+            layer4_weights = self.weight_variable([split_by_half(hidden_nodes, 2), output_layer_size])
+            layer4_biases = self.bias_variable([output_layer_size])
 
-            keep_prob = tf.placeholder("float")
+            self.tf_keep_prob = tf.placeholder("float")
 
             # Model with dropout
-            def model(data, proba=keep_prob):
+            def model(data, proba=self.tf_keep_prob):
                 layer1 = tf.matmul(data, layer1_weights) + layer1_biases
                 hidden1 = tf.nn.dropout(tf.nn.relu(layer1), proba)  # dropout on hidden layer
                 layer2 = tf.matmul(hidden1, layer2_weights) + layer2_biases  # a new hidden layer
@@ -65,8 +74,8 @@ class Baseline:
                 return tf.matmul(hidden3, layer4_weights) + layer4_biases
 
             # Training computation.
-            logits = model(tf_train_dataset, keep_prob)
-            loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
+            logits = model(self.tf_train_dataset, self.tf_keep_prob)
+            loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, self.tf_train_labels))
             regularizers = (tf.nn.l2_loss(layer1_weights) + tf.nn.l2_loss(layer1_biases) + \
                             tf.nn.l2_loss(layer2_weights) + tf.nn.l2_loss(layer2_biases) + \
                             tf.nn.l2_loss(layer3_weights) + tf.nn.l2_loss(layer3_biases) + \
@@ -74,27 +83,61 @@ class Baseline:
 
             # Add the regularization term to the loss.
             # loss += lamb_reg * regularizers
-            loss = tf.reduce_mean(loss + lamb_reg * regularizers)
+            self.loss = tf.reduce_mean(loss + lamb_reg * regularizers)
 
             # Optimizer.
             # learning rate decay
-            global_step = tf.Variable(0)  # count  number of steps taken.
-            start_learning_rate = 0.5
-            learning_rate = tf.train.exponential_decay(start_learning_rate, global_step, 100000, 0.96, staircase=True)
-            optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+            self.global_step = tf.Variable(0)  # count  number of steps taken.
+
+            learning_rate = tf.train.exponential_decay(start_learning_rate, self.global_step, 100000, 0.96, staircase=True)
+            self.optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=self.global_step)
 
             # Predictions for the training, validation, and test data.
-            train_prediction = tf.nn.softmax(logits)
-            valid_prediction = tf.nn.softmax(model(tf_valid_dataset, 1.0))
-            test_prediction = tf.nn.softmax(model(tf_test_dataset, 1.0))
+            self.train_prediction = tf.nn.softmax(logits)
+            self.test_prediction = tf.nn.softmax(model(self.tf_test_dataset, 1.0))
+
+            print('Training prediction: ', self.train_prediction)
+            print('Test prediction: ', self.test_prediction)
+
+    def accuracy(self, predictions, labels):
+        return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+                    / predictions.shape[0])
+
+    def run_session(self, num_epochs, name, k_prob=1.0):
+
+        with tf.Session(graph= self.graph) as session:
+            merged = tf.merge_all_summaries()
+            writer = tf.train.SummaryWriter("/tmp/tensorflowlogs", session.graph)
+            tf.initialize_all_variables().run()
+            print("Initialized")
+            for epoch in range(num_epochs):
+                offset = (epoch * self._batch_size) % (self._ds.train_labels.shape[0] - self._batch_size)
+                batch_data = self._ds.train_dataset[offset:(offset + self._batch_size), :]
+                batch_labels = self._ds.train_labels[offset:(offset + self._batch_size), :]
+                feed_dict = {self.tf_train_dataset: batch_data, self.tf_train_labels: batch_labels, self.tf_keep_prob: k_prob}
+
+                _, l, predictions = session.run([self.optimizer, self.loss, self.train_prediction], feed_dict=feed_dict)
+
+                if (epoch % 500 == 0):
+                    print("Minibatch loss at epoch {}: {}".format(epoch, l))
+                    print("Minibatch accuracy: {:.1f}".format(self.accuracy(predictions, batch_labels)))
+                    #print("Validation accuracy: {:.1f}".format(self.accuracy(self.valid_prediction.eval(), self.valid_labels)))
+            print("Test accuracy: {:.1f}".format(self.accuracy(self.test_prediction.eval(), self._ds.test_labels)))
+            #self.test_preds[name] = self.test_prediction.eval().ravel()
 
 
 
 
 def main():
-    d = DataSet()
+    d = ds.DataSet()
     d.load()
 
+    b = Baseline(d)
+    b.create()
+
+    start = time.time()
+    b.run_session(10000, "NN")
+    print("Completed in {} minutes".format((time.time()-start)/60))
     # Visualise the data for one image
     #d.display(40000)
 